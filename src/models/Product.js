48f884c4a48e1d05dc61fbe45a9a9ec6f3c943b2
hppod <hppodgornik@gmail.com>
2018-11-22 01:36:29 -0200

    corrigindo erros

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoosePaginate = require('mongoose-pagination')
+const mongoosePaginate = require('mongoose-paginate')
 
 const ProductSchema = new mongoose.Schema({
     title: {
