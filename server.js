c3d2fbc060798afa3dd73a9b1ab36589e6af3e17
hppod <hppodgornik@gmail.com>
2018-11-22 00:52:12 -0200

    criando um product

--- server.js
+++ server.js
@@ -10,8 +10,15 @@ mongoose.connect('mongodb://192.168.99.100:27017/nodeapi', {
 
 requireDir('./src/models');
 
+const Product = mongoose.model('Product');
+
 app.get('/', (req, res) => {
-    res.send('Hello Rocketseat')
+    Product.create({
+        title: 'Angular',
+        description: 'Build applications with Angular',
+        url: 'https://angular.io'
+    });
+    return res.send('Hello Rocketseat')
 });
 
-app.listen(3001);
+app.listen(3001);   
