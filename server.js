4dd7ac1b703853ef491f00e041b9b6f5d676019f
hppod <hppodgornik@gmail.com>
2018-11-22 00:44:06 -0200

    adicionando o mongoose

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://192.168.99.100:27017/nodeapi', {
+    useNewUrlParser: true
+});
 
 app.get('/', (req, res) => {
     res.send('Hello Rocketseat')
