b9f22f7ec4a1025a010b06b98973423855f59436
hppod <hppodgornik@gmail.com>
2018-11-22 00:49:31 -0200

    atualizando pra usar require-dir

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ mongoose.connect('mongodb://192.168.99.100:27017/nodeapi', {
     useNewUrlParser: true
 });
 
-require('./src/models/Product');
+requireDir('./src/models');
 
 app.get('/', (req, res) => {
     res.send('Hello Rocketseat')
